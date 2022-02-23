42facfec3e453b74494526b4683a4ed8d2fa19eb
Radovan Haluska <radovan.haluska1@gmail.com>
2022-02-23 20:42:18 +0100

    Add a support for hyperparameters in preprocessing methods

--- src/core/preprocessings/resampler.py
+++ src/core/preprocessings/resampler.py
@@ -1,5 +1,17 @@
+import itertools
+from typing import Any, Generator
+
 import numpy as np
 
 class Resampler:
+    _hyperparams = {}
+
+    @classmethod
+    def hyperparams(cls) -> Generator[dict[str, Any], None, None]:
+        keys = cls._hyperparams.keys()
+
+        for conf in itertools.product(*cls._hyperparams.values()):
+            yield dict(zip(keys, conf))
+
     def fit_resample(self, data: np.ndarray, target: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         raise NotImplementedError()
