fc3cfc91454c4a859b9a14e3d8b0b11a2188d127
Radovan Haluska <radovan.haluska1@gmail.com>
2022-03-06 09:40:03 +0100

    Add type hints and override methods in the baseline preprocessing method

--- src/core/preprocessings/baseline.py
+++ src/core/preprocessings/baseline.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 import numpy as np
 
 from core.preprocessings.resampler import Resampler
 
 class BaselineResampler(Resampler):
-    def __init__(self, random_state: int) -> None:
-        pass # Only for compatibility with other preprocessing methods
-
     def fit_resample(self, data: np.ndarray, target: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         return data, target
+
+    def get_params(self, deep: bool = True) -> dict[str, Any]:
+        return {}
