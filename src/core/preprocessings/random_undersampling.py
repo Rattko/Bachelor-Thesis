15f71d28f680b7e736eb52d27a1314f3eb95807a
Radovan Haluska <radovan.haluska1@gmail.com>
2022-03-06 10:04:02 +0100

    Perform minor changes in the random undersampling preprocessing method

--- src/core/preprocessings/random_undersampling.py
+++ src/core/preprocessings/random_undersampling.py
@@ -1,4 +1,3 @@
-import numpy as np
 from imblearn.under_sampling import RandomUnderSampler
 
 from core.preprocessings.resampler import Resampler
@@ -9,10 +8,9 @@ class RandomUndersamplingResampler(Resampler):
     }
 
     def __init__(self, sampling_strategy: float, random_state: int) -> None:
+        super().__init__()
+
         self.resampler = RandomUnderSampler(
             sampling_strategy=sampling_strategy,
             random_state=random_state
         )
-
-    def fit_resample(self, data: np.ndarray, target: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-        return self.resampler.fit_resample(data, target)
