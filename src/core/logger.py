68a36eec089197398f990087b0e7eab839fa208c
Radovan Haluska <radovan.haluska1@gmail.com>
2022-04-08 15:42:46 +0200

    Add a constructor to the Logger class

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,13 +1,22 @@
 import mlflow
 
+from core.utils import get_run_name
+
+
 class Logger:
-    def log_params(self, prefix, params) -> None:
+    def __init__(self, dataset_name: str, preproc_name: str) -> None:
+        self.dataset_name = dataset_name
+        self.preproc_name = preproc_name
+
+    def log_params(self, prefix: str, params) -> None:
         # Log namespaced hyperparameters into MLFlow
         mlflow.log_params({f'{prefix}__{key}': value for key, value in params.items()})
 
-    def log_model(self, model, model_name) -> None:
+    def log_model(self, model) -> None:
         # Pickle the trained AutoSklearn model into MLFlow
-        mlflow.sklearn.log_model(model, 'model', registered_model_name=model_name)
+        mlflow.sklearn.log_model(
+            model, 'model', registered_model_name=get_run_name(self.dataset_name, self.preproc_name)
+        )
 
     def log_preds(self, data, target, preds) -> None:
         file_name = 'preds.json' if len(preds.shape) == 1 else 'preds_proba.json'
